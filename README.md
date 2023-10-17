0c10a3f11b2911b497896ba5a1b30116f273c9d2
Your Name <you@example.com>
2023-10-17 03:32:53 +0300

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+## server
+
+https://github.com/netology-code/ra16-homeworks/tree/ra-51/router/crud/backend
+
---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { HashRouter, Route, Routes, Link } from "react-router-dom";
+import './App.css';
+import ShiftPost from './components/ShiftPost';
+import Posts from './components/Posts';
+import SelectPost from './components/SelectPost';
+
+export default function App() {
+  return (
+    <div className='App'>
+      <HashRouter>
+
+        <nav className="nav">
+          <div className='head-title'>
+            <Link to="/posts/new">
+              <button className="btn-add">Создать пост</button>
+            </Link>
+          </div>
+        </nav>
+
+        <div className="page">
+          <Routes>
+            <Route path="/posts" element={<Posts />} />
+            <Route path="/posts/new" element={<ShiftPost title={'Создание'}/>} />
+            <Route path="/posts/:id" element={<SelectPost />} />
+            <Route path="*" element={<Posts />} />
+          </Routes>
+        </div>
+
+      </HashRouter>
+    </div>
+  );
+}
